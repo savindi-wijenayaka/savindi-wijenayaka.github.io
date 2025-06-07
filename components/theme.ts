1914828ee02ace793ab182a586b798a54d2f35e3
Savindi Wijenayaka <savindi.narmada@gmail.com>
2025-06-07 17:39:33 +1200

    Update theme

--- components/theme.ts
+++ components/theme.ts
@@ -10,17 +10,18 @@ const config = defineConfig({
     tokens: {
       colors: {
         brand: {
-          50: { value: "#FCFCFC" },
-          100: { value: "#F8F0E8" },
-          200: { value: "#EFDDCA" },
-          300: { value: "#E8CFB5" },
-          400: { value: "#AAA89D" },
-          500: { value: "#62797A" },
-          600: { value: "#657E7D" },
-          700: { value: "#39626A" },
-          800: { value: "#1D3D4E" },
-          900: { value: "#1C3245" },
-          950: { value: "#1A263C" },
+          // 50: { value: "#FFFFFF" },
+          50: { value: "#EDEDF7" },
+          100: { value: "#DFDEF2" },
+          200: { value: "#CBB4E2" },
+          300: { value: "#6c69a8" },
+          400: { value: "#999CD6" },
+          500: { value: "#B789D1" },
+          600: { value: "#9540A0" },
+          700: { value: "#5C2E76" },
+          800: { value: "#292C66" },
+          900: { value: "#0E0D21" },
+          950: { value: "#080812" },
         },
       },
       fonts: {
@@ -32,7 +33,7 @@ const config = defineConfig({
       colors: {
         brand: {
           solid: {
-            value: { _light: "colors.brand.800", _dark: "colors.brand.200" },
+            value: { _light: "colors.brand.800", _dark: "colors.brand.400" },
           },
           contrast: {
             value: { _light: "colors.brand.100", _dark: "colors.brand.900" },
@@ -53,10 +54,10 @@ const config = defineConfig({
             value: { _light: "colors.brand.950", _dark: "colors.brand.50" },
           },
           fgEmph: {
-            value: { _light: "colors.brand.600", _dark: "colors.brand.300" },
+            value: { _light: "colors.brand.600", _dark: "colors.brand.500" },
           },
           fgMuted: {
-            value: { _light: "colors.brand.500", _dark: "colors.brand.400" },
+            value: { _light: "colors.brand.300", _dark: "colors.brand.400" },
           },
           bg: {
             value: { _light: "colors.brand.50", _dark: "colors.brand.950" },
