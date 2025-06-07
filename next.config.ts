d74288abcd58c99c87837e1cd2583a10822cfce3
Savindi Wijenayaka <savindi.narmada@gmail.com>
2025-06-07 20:18:03 +1200

    Update NextJs configs for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/savindi-wijenayaka.github.io",
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
